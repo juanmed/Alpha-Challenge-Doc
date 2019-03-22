3c18acd6d6a1069741ad9102bf77e941f8f6bf81
egauh0113 <egauh@naver.com>
2019-03-23 04:56:12 +0900

    Update install.bash

--- Test3/install.bash
+++ Test3/install.bash
@@ -1,2 +1,2 @@
 python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
-git clone https://github.com/cvxopt/cvxopt.git && cd /cvxopt && python setup.py install --user
+git clone https://github.com/cvxopt/cvxopt.git && cd cvxopt && python setup.py install --user
